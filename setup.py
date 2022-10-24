a8ab8085beafcd3323b273d7bbcd1b3c9873862d
Kelsey Mok <kelseymok@gmail.com>
2022-10-24 12:34:21 +0200

    adding  location of src files

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+from os.path import basename
+from os.path import splitext
+from glob import glob
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
@@ -11,5 +14,7 @@ setup(
     author_email='kelseymok@gmail.com',
     url='https://github.com/kelseymok/ocpp_auth_key',
     packages=find_packages(exclude=('tests')),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     install_requirements=required
 )
